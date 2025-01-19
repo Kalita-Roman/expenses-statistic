794cb6150f089db2c2ea77d64993bda75fdc5e1f
Raman Kalita <raman_kalita@epam.com>
2025-01-19 10:06:54 +0100

    Manage sessions

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,24 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import PostgresAdapter from "@auth/pg-adapter"
+// import { Pool } from "@neondatabase/serverless"
+import { Pool } from "pg"
+
+// const pool = new Pool({ connectionString: process.env.POSTGRES_URL })
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  ssl: true
+})
 
 
 export const authConfig = {
+  adapter: PostgresAdapter(pool),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
