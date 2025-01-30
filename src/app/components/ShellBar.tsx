b4739075fe18d3e1c49df455560368ec1d786ae0
Роман <roman.kalita.1989@gmail.com>
2025-01-30 21:56:31 +0000

    Fix Types

--- src/app/components/ShellBar.tsx
+++ src/app/components/ShellBar.tsx
@@ -1,26 +1,40 @@
 import { ReactNode } from "react";
 
-export const ShellBar = ({ session, signIn, signOut }: { session: unknown, signIn: () => Promise<void>, signOut: () => Promise<void> }) => {
+type Session = {
+  user?: {
+    image: string;
+  };
+};
 
+export const ShellBar = ({
+  session,
+  signIn,
+  signOut,
+}: {
+  session: Session;
+  signIn: () => Promise<void>;
+  signOut: () => Promise<void>;
+}) => {
   return (
     <div className="shell-bar bg-gray-800 text-white p-4 flex justify-end items-center ml-auto">
-      {!session?.user && <form
+      {!session?.user && (
+        <form
           action={async () => {
-            "use server"
-            await signIn()
+            "use server";
+            await signIn();
           }}
         >
           <Button>Sign In</Button>
         </form>
-      }
+      )}
       {session?.user?.image && (
         <>
-           {/* eslint-disable-next-line @next/next/no-img-element */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img src={session.user.image} alt="avatar" className="w-8 h-8" />
           <form
             action={async () => {
-              "use server"
-              await signOut()
+              "use server";
+              await signOut();
             }}
             className="flex ml-2"
           >
@@ -32,4 +46,8 @@ export const ShellBar = ({ session, signIn, signOut }: { session: unknown, signI
   );
 };
 
-const Button = ({ children }: { children: ReactNode }) => <button className="bg-transparent hover:bg-gray-900 text-white-700 font-semibold hover:text-white py-1 px-4 border border-white-500 hover:border-transparent rounded-full">{children}</button>;
+const Button = ({ children }: { children: ReactNode }) => (
+  <button className="bg-transparent hover:bg-gray-900 text-white-700 font-semibold hover:text-white py-1 px-4 border border-white-500 hover:border-transparent rounded-full">
+    {children}
+  </button>
+);
