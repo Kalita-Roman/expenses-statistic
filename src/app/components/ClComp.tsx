47604e8f4ddc03999fdf43dee9b871c689714e1b
Роман <roman.kalita.1989@gmail.com>
2025-01-31 19:38:06 +0000

    Test

---
+++ src/app/components/ClComp.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+
+const ClComp: React.FC = () => {
+  console.log("\n\n\n>>> ClComp\n\n\n");
+  return <div>Hello, World!</div>;
+};
+
+export default ClComp;
