ca3084a66d56dfe133751c6ce4594bd75822919f
Raman Kalita <raman_kalita@epam.com>
2025-02-16 13:39:21 +0100

    Create the first tables

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,49 +1,9 @@
 -- CreateTable
-CREATE TABLE "accounts" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "type" VARCHAR(255) NOT NULL,
-    "provider" VARCHAR(255) NOT NULL,
-    "providerAccountId" VARCHAR(255) NOT NULL,
-    "refresh_token" TEXT,
-    "access_token" TEXT,
-    "expires_at" BIGINT,
-    "id_token" TEXT,
-    "scope" TEXT,
-    "session_state" TEXT,
-    "token_type" TEXT,
-
-    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "sessions" (
-    "id" SERIAL NOT NULL,
-    "userId" INTEGER NOT NULL,
-    "expires" TIMESTAMPTZ(6) NOT NULL,
-    "sessionToken" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "users" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(255),
-    "email" VARCHAR(255),
-    "emailVerified" TIMESTAMPTZ(6),
-    "image" TEXT,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "auth_id" INTEGER NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "verification_token" (
-    "identifier" TEXT NOT NULL,
-    "expires" TIMESTAMPTZ(6) NOT NULL,
-    "token" TEXT NOT NULL,
-
-    CONSTRAINT "verification_token_pkey" PRIMARY KEY ("identifier","token")
 );
 
 -- CreateTable
@@ -54,4 +14,28 @@ CREATE TABLE "tags" (
 
     CONSTRAINT "tags_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" VARCHAR(255) NOT NULL,
+    "note" TEXT,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "expenses" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "date" DATE NOT NULL,
+    "amount" DECIMAL(15,2) NOT NULL,
+    "currency" CHAR(3) NOT NULL,
+    "note" TEXT,
+    "category" UUID,
+
+    CONSTRAINT "expenses_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "expenses" ADD CONSTRAINT "fk_category" FOREIGN KEY ("category") REFERENCES "category"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
 
