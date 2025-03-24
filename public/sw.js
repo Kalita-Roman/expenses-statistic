fb5edd459c276f115cf0534cfaafe73c4e53b201
Raman Kalita <raman_kalita@epam.com>
2025-03-24 10:51:32 +0100

    Update icons

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-self.addEventListener('install', (event) => {
-  console.log('Service Worker. install');
+self.addEventListener("install", (event) => {
+  console.log(">>> Service Worker. install");
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker. activate');
+self.addEventListener("activate", (event) => {
+  console.log(">>> Service Worker. activate");
 });
 
-self.addEventListener('fetch', (event) => {
-  console.log('Service Worker. fetching:', event.request.url);
+self.addEventListener("fetch", (event) => {
+  console.log(">>> Service Worker. fetching:", event.request.url);
   event.respondWith(fetch(event.request));
 });
