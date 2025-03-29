b4a0aec2897c42db95feb93902c39c4b90d5284a
Raman Kalita <raman_kalita@epam.com>
2025-03-29 07:37:35 +0100

    Update layout to use dynamic viewport height for better responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-dvh`}
       >
         <ShellBar />
         <div className="flex-grow h-full">
